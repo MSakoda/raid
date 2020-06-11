83fb6eaef256d20a4c7c3cdc52786487db57a002
Marcus Sakoda <msakoda@MACMSAKMBP15A.local>
2020-06-10 19:25:06 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'raid';
+
+  champions: any = [
+    {
+      name: 'champ 1',
+      exp: 0
+    },
+    {
+      name: 'champ 2',
+      exp: 0
+    },
+  ];
+
+  onClickChampion(champion) {
+    console.log(`clicked champion`);
+    champion.exp += 1;
+  }
+
+  buyChampion() {
+    let newChamp = {
+      name: `champ ${this.champions.length + 1}`,
+      exp: 0
+    }
+
+    this.champions.push(newChamp);
+  }
+
 }
